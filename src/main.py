409066f5767c6338597d49ae0446668e6d00c615
ericles-porty <ericlesdsantos@gmail.com>
2022-12-21 17:17:55 -0300

    Add Class Diagram

---
+++ src/main.py
@@ -0,0 +1,19 @@
+dic = {}
+
+line = []
+line.append(('Poço Redondo', 20.9))
+dic['Canidé'] = line
+
+line = []
+line.append(('Canidé', 20.9))
+line.append(('Porto da Folha', 66.3))
+line.append(('Feira Nova', 71.7))
+dic['Poço Redondo'] = line
+
+line = []
+line.append(('Propriá',81.2))
+line.append(('Poço Redondo', 66.3))
+line.append(('Feira Nova', 53.8))
+dic['Porto da Folha'] = line
+
+print(dic)
